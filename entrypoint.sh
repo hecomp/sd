f16977256c85620a86f8862cadfd67d14490e2c4
hecomp <heber.silva@aura.com>
2021-10-07 12:52:53 -0400

    modified workflow

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,7 @@ echo $GITHUB_SHA
 echo "::set-degug name=sha::$GITHUB_SHA"
 
 # ## 
-URL="https://api.github.com/repos/${{ github.repository }}/pulls/${{ github.event.pull_request.number }}/files"
+URL="https://api.github.com/repos/${GITHUB_REPO}/pulls/${GITHUB_EVENT_PR_NUMER}/files"
 FILES=$(curl -s -X GET -G $URL | jq -r '.[] | .filename')
 if echo $FILES | grep -q ".yml"; then
 echo "json file changed!!"
